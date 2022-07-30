ee94dd5589af2334f29f5ff99b6987e05c368cee
Yoon Chan ho <cnsj72@gmail.com>
2022-07-30 22:30:37 +0900

    2022-07-90 클래스 1강

--- 클래스/Program.cs
+++ 클래스/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace 클래스
 {
+    // internal 생략된 형태
+    class Date
+    {
+        /*int Day;
+        public int Year, Month;*/
+
+        public int Year;
+        protected int Month;
+        int Day;
+        //  Month 와 Day는 내부에서만 사용 가능
+
+        public void Print()
+        {
+            Console.WriteLine("{0} {1} {2}", Year, Month, Day);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Date Test = new Date();
+            Test.Print();
         }
     }
 }
